7eb5d345723748e034fc9ea2dbbf240ba7c4bd4f
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-02-09 20:02:25 +0100

    Configure proxy for API

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,17 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/hello', (_, res) => {
+  res.json({ message: 'Hello World' });
+});
 
 if (process.env.NODE_ENV === 'production') {
   app.use('/assets', express.static(path.join(__dirname, '../../dist')));
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import P from '../components/P/P';
 import Heading from '../components/Heading/Heading';
 
-const HomePage = () => (
-  <div>
-    <Heading>Webpack config for React</Heading>
-    <P>Boostrap your SPA React project</P>
-  </div>
-);
+const HomePage = () => {
+  const [data, setData] = React.useState();
+  return (
+    <div>
+      <Heading>Webpack config for React</Heading>
+      <P>Boostrap your SPA React project</P>
+      <button
+        type="button"
+        onClick={async () => {
+          const response = await fetch('/api/hello');
+          const incomingData = await response.json();
+          console.log(incomingData);
+          setData(incomingData);
+        }}
+      >
+        API
+      </button>
+      <div>{data ? data.message : null}</div>
+    </div>
+  );
+};
 
 export default HomePage;
