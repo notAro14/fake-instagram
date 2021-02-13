a49441c00cd14d044c92ab7380505ee6bfcfb723
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-02-13 16:01:58 +0100

    Reset UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './views/HomePage';
 
-import Loading from './components/Loading/Loading';
-import Layout from './components/Layout/Layout';
-
-const HomePage = lazy(() =>
-  import(/* webpackChunkName: "HomePage" */ './views/HomePage')
-);
-const InstallationPage = lazy(() =>
-  import(/* webpackChunkName: "InstallationPage" */ './views/InstallationPage')
-);
-const NotFoundPage = lazy(() =>
-  import(/* webpackChunkName: "NotFoundPage" */ './views/NotFoundPage')
-);
-
-export default () => {
+const App = () => {
   return (
     <Router>
-      <Layout>
-        <Suspense fallback={<Loading />}>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/installation" component={InstallationPage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </Suspense>
-      </Layout>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </Router>
   );
 };
+
+export default App;
