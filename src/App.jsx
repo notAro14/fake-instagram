c7d1311096a2bb6c348f78df8da59acc749ceb7b
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-02-14 01:13:35 +0100

    Add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './views/Home/Home';
+import NotFound from './views/NotFound/NotFound';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Layout>
+      <Layout>
+        <Switch>
           <Route exact path="/" component={Home} />
-        </Layout>
-      </Switch>
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
