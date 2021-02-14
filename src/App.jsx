d3a10db629e0a2274366fdd55f4c3700d17b1aaa
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-02-14 01:24:50 +0100

    Add code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import Home from './views/Home/Home';
-import NotFound from './views/NotFound/NotFound';
+// import Home from './views/Home/Home';
+// import NotFound from './views/NotFound/NotFound';
+
+const Home = lazy(() =>
+  import(/* webpackChunkName: "Home" */ './views/Home/Home')
+);
+
+const NotFound = lazy(() =>
+  import(/* webpackChunkName: "NotFound" */ './views/NotFound/NotFound')
+);
 
 const App = () => {
   return (
     <Router>
       <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
       </Layout>
     </Router>
   );
