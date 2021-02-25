e813b472cd15ba0ddabedc150c77a87341e1351f
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-02-26 00:27:31 +0100

    Add Naruto run when loading home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import naruto from 'animexyz';
+import { useLocation } from 'react-router-dom';
 import { HomeWrapper } from './Home.style';
 import Aside from './Aside';
 import Main from './Main';
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => naruto({ duration: 2000 }), [location]);
   return (
     <HomeWrapper>
       <Main />
