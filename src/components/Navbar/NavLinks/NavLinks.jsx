a483d9864e55b10ecc4ed4b65ac88abb35f128bc
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-03-14 18:21:22 +0100

    Add Publish button and refactor

--- src/components/Navbar/NavLinks/NavLinks.jsx
+++ src/components/Navbar/NavLinks/NavLinks.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { AiFillHome } from 'react-icons/ai';
 import { CgProfile } from 'react-icons/cg';
+import { MdPhotoCamera } from 'react-icons/md';
 import NavLink from './NavLink';
 import { NavLinksWrapper } from './NavLinks.style';
 
+const navLinks = [
+  {
+    to: '/',
+    key: 'home',
+    icon: <AiFillHome />,
+  },
+  {
+    to: '#',
+    key: 'publish',
+    icon: <MdPhotoCamera />,
+  },
+  {
+    to: '/profile',
+    key: 'profile',
+    icon: <CgProfile />,
+  },
+];
+
 const NavLinks = () => (
   <NavLinksWrapper>
-    <NavLink to="/">
-      <AiFillHome />
-    </NavLink>
-    <NavLink to="/profile">
-      <CgProfile />
-    </NavLink>
+    {navLinks.map(({ to, icon, key }) => (
+      <NavLink key={key} to={to}>
+        {icon}
+      </NavLink>
+    ))}
   </NavLinksWrapper>
 );
 
