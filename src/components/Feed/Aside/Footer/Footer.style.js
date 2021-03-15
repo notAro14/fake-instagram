37758c0cd4ada6925e330e1e4f414a240559e6f6
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-03-15 23:33:44 +0100

    Update Footer style

--- src/components/Feed/Aside/Footer/Footer.style.js
+++ src/components/Feed/Aside/Footer/Footer.style.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const FooterWrapper = styled.footer`
+  line-height: 2;
+  margin-top: 20px;
   color: var(--text-secondary);
   a {
     color: inherit;
