c872b0fb2dcd16741fb39c281f1479fb95da2c2d
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-05-29 22:21:59 +0200

    Improve readability

--- src/server/models/post.js
+++ src/server/models/post.js
@@ -4,11 +4,11 @@ const { Schema, model } = mongoose
 
 const postSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
-  userId: { type: String, required: true },
   description: { type: String, required: true },
   hearts: { type: Number, default: 0 },
   image: String,
   title: { type: String, required: true },
+  userId: { type: String, required: true },
 })
 
 const Post = model('Post', postSchema)
