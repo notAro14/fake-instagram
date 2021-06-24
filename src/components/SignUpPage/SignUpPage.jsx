dd532c65ba79eb87bbd6787e031c8e1f944fc908
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-06-24 23:06:45 +0200

    Refactor SignUpForm

--- src/components/SignUpPage/SignUpPage.jsx
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import { CatchPhrase, Title, PrimaryLink, Box } from '../common'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useUser } from 'Context/user.context'
+import { CatchPhrase, Title, PrimaryLink, Box, Spinner } from '../common'
 import SignUpForm from '../SignUpForm'
+import notify from '../../helpers/notification'
+import { signup } from '../../api/user'
+import { LOADING, IDLE } from '../../constants'
 
 const SignUpPage = () => {
+  const history = useHistory()
+  const { setAuthState } = useUser()
+
+  const [state, setState] = useState(IDLE)
+
+  const onSubmit = async ({ email, password, username, displayname }) => {
+    setState(LOADING)
+    signup({ email, password, username, displayname }).then(
+      (user) => {
+        setState(IDLE)
+        setAuthState(user)
+        history.push('/')
+        notify.success(`Welcome ${user.userInfo.displayname.split(' ')[0]}`)
+      },
+      (error) => {
+        notify.error(error.message)
+        setState(IDLE)
+      }
+    )
+  }
+
   return (
     <>
       <Box>
@@ -10,7 +36,8 @@ const SignUpPage = () => {
         <CatchPhrase>
           Sign up to see your friend&apos;s photos and videos.
         </CatchPhrase>
-        <SignUpForm />
+        <SignUpForm onSubmit={onSubmit} />
+        {state.label === LOADING && <Spinner />}
       </Box>
       <Box>
         <p>
