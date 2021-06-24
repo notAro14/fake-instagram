a0048a2a9dd83eb17a81542a77e663b798a5963a
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-06-24 22:30:10 +0200

    Write test for SignInForm

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,45 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import * as yup from 'yup'
+import { func } from 'prop-types'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
-import { useHistory } from 'react-router-dom'
-import { Button, SimpleInput, Password, FormWrapper, Spinner } from '../common'
-import { useUser } from '../../context/user.context'
-import { login } from '../../api/user'
-import { LOADING, IDLE } from '../../constants'
-import notify from '../../helpers/notification'
+import { Button, SimpleInput, Password, FormWrapper } from '../common'
 
 const schema = yup.object().shape({
   email: yup.string().email().required('Enter a valid email'),
   password: yup.string().required('Enter a valid password'),
 })
 
-const SignInForm = () => {
+const SignInForm = ({ onSubmit }) => {
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm({ mode: 'onBlur', resolver: yupResolver(schema) })
-  const [state, setState] = useState(IDLE)
-  const { setAuthState } = useUser()
-  const history = useHistory()
-  const onSubmit = ({ email, password }) => {
-    setState(LOADING)
-    login({ email, password }).then(
-      (user) => {
-        setState(IDLE)
-        setAuthState(user)
-        history.push('/')
-        notify.success(
-          `Welcome back ${user.userInfo.displayname.split(' ')[0]} !`
-        )
-      },
-      (error) => {
-        notify.error(error.message)
-        setState(IDLE)
-      }
-    )
-  }
 
   return (
     <FormWrapper
@@ -54,9 +30,12 @@ const SignInForm = () => {
         Password
       </Password>
       <Button type='submit'>Sign in</Button>
-      {state.label === LOADING && <Spinner />}
     </FormWrapper>
   )
+}
+
+SignInForm.propTypes = {
+  onSubmit: func.isRequired,
 }
 
 export default SignInForm
