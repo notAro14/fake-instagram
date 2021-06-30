6544c3ed0ce216f2bf36d82827d643d563883a65
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-06-30 23:39:27 +0200

    Improve Post model

--- src/server/models/post.js
+++ src/server/models/post.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose'
 
-const { Schema, model } = mongoose
+const { Schema, model, Types } = mongoose
 
 const postSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
   description: { type: String, required: true },
-  hearts: { type: [String], default: [] },
+  hearts: { type: [Types.ObjectId], default: [] },
   image: String,
   title: { type: String, required: true },
-  userId: { type: String, required: true },
+  userId: { type: Types.ObjectId, required: true },
 })
 
 const Post = model('Post', postSchema)
